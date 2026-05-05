f525b8327976694256e5631ae54da336628d78cf
egorkharaim <egor.kharaima@gmail.com>
2026-05-05 13:38:05 +0300

    feat: implement mappers for Student, Group and Subject

--- src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
@@ -3,12 +3,48 @@ package mate.academy.mapstruct.mapper;
 import mate.academy.mapstruct.dto.student.CreateStudentRequestDto;
 import mate.academy.mapstruct.dto.student.StudentDto;
 import mate.academy.mapstruct.dto.student.StudentWithoutSubjectsDto;
+import mate.academy.mapstruct.model.Group;
 import mate.academy.mapstruct.model.Student;
+import mate.academy.mapstruct.model.Subject;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
+@Mapper(componentModel = "spring")
 public interface StudentMapper {
+    @Mapping(target = "groupId", source = "group.id")
+    @Mapping(target = "subjectIds", source = "subjects")
     StudentDto toDto(Student student);
 
+    @Mapping(target = "groupId", source = "group.id")
     StudentWithoutSubjectsDto toStudentWithoutSubjectsDto(Student student);
 
+    @Mapping(target = "subjects", source = "subjects")
+    @Mapping(target = "group", source = "groupId", qualifiedByName = "groupById")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "socialSecurityNumber", ignore = true)
     Student toModel(CreateStudentRequestDto requestDto);
+
+    @Named("groupById")
+    default Group groupById(Long id) {
+        if (id == null) {
+            return null;
+        }
+        Group group = new Group();
+        group.setId(id);
+        return group;
+    }
+
+    default Subject mapIdToSubject(Long id) {
+        if (id == null) {
+            return null;
+        }
+        Subject subject = new Subject();
+        subject.setId(id);
+        return subject;
+    }
+
+    default Long mapSubjectToId(Subject subject) {
+        return (subject != null) ? subject.getId() : null;
+    }
 }
--- src/main/java/mate/academy/mapstruct/model/Subject.java
+++ src/main/java/mate/academy/mapstruct/model/Subject.java
@@ -5,12 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "subjects")
 public class Subject {
     @Id
